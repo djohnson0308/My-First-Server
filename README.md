4c64ca04254cad5bcf2afe01b959731a150ab1d6
Devon Johnson <dljohnson@rollins.edu>
2017-01-04 11:31:34 -0500

    first server

---
+++ README.md
@@ -0,0 +1,1 @@
+This is my very first Sinatra web server. To initialize it go into Terminal and run ruby server.rb
---
+++ public/hello.js
@@ -0,0 +1,11 @@
+
+console.log("Hello world!");
+
+var input;
+console.log(input);
+
+if (input === undefined) {
+    console.log("It's undefined!");
+} else {
+    console.log(input);
+}
